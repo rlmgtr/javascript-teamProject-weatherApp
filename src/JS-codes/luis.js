0d41af156607c1fb8cac92f2952b58da3ba61ea5
rlmgtr <rlmagtira@gmail.com>
2024-05-21 13:52:49 +0800

    added details for five days

--- src/JS-codes/luis.js
+++ src/JS-codes/luis.js
@@ -1,6 +1,10 @@
-function getFiveDaysWeather() {
-    const cityInput = document.getElementById("inputcity");
-    const cityName = document.getElementById("cityname");
-    cityName.innerHTML = cityInput.value 
+function getFiveDaysWeather(){
+    const cityInput= document.getElementById("cityInput");
+    const cityName= document.getElementById("cityName");
+    cityName.innerHTML = "City of "+cityInput.value+"" 
 }
 
+Fetch("https://api.openweathermap.org/data/2.5/weather?q=+cityInput.value+&appid=f1a7f601f87c9d97579ef8237cc83ff1")
+
+.then(response => response.json())
+.then(data => {})
