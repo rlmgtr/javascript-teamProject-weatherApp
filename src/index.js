64e33d82bd0d8d296d9e41bea82dcfc77f2f0ae0
rlmgtr <rlmagtira@gmail.com>
2024-05-22 22:18:07 +0800

    update

--- src/index.js
+++ src/index.js
@@ -12,3 +12,33 @@ document.getElementById('tWeather').addEventListener('click', function(){
     document.getElementById('FiveDaysWeather').style.display = 'none';
 } )
 
+//// ////////////
+
+document.getElementById('cityInputForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const cityInput = document.getElementById('cityInput').value;
+    fetchweather(cityInput);
+});
+
+function fetchweather(cityInput) {
+    const apiKey = 'f1a7f601f87c9d97579ef8237cc83ff1';
+    const apiURL = 'f1a7f601f87c9d97579ef8237cc83ff1';
+
+    fetch(apiURL)
+    .then(response => response.json())
+    .then(data => {
+        const fiveForecast = data.list.reduce((acc, item) => {
+            const date = item.dt_txt.split(' ')[0];
+            if (!acc[date]) {
+                acc[date] = [];
+            }
+            acc[date].push(item);
+            return acc;
+        }, {});
+
+        displayForcast(forecast-info)
+    })
+    .catch(error => {
+        console.error('Error loading weather...', error);
+    });
+}
